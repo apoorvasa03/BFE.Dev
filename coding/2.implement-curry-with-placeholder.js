ad2b10c8fc40ffe759e83e9ddd0c7c664032b069
apoorvasa03 <apoorvasa03@gmail.com>
2022-02-11 12:33:31 -0800

    - curry

--- coding/2.implement-curry-with-placeholder.js
+++ coding/2.implement-curry-with-placeholder.js
@@ -1,28 +1,22 @@
 function curry(func){
     return function curried(...args){
         if(!args.includes(curry.placeholder) && args.length >= func.length) return func(...args)
-        else{
-            return function(newArgs){
-                let res = args.map(arg => arg === curry.placeholder && newArgs.length ? newArgs.shift(): arg)
-                return curried(...res, ...newArgs)
-            }
-        }
+        return (...newArgs) => {
+            let res = args.map(arg => arg === curry.placeholder && newArgs.length ? newArgs.shift() : arg)
+            return curried(...res, ...newArgs)
+        } 
     }
 }
 
-let join = (a, b, c) => {
+const  join = (a, b, c) => {
     return `${a}_${b}_${c}`
-}
-
-let curriedJoin = curry(join)
-
-
-
-function curry (func){
-    return function curried(...args){
-        if(args.length >= func.length) return func(...args)
-        else (...newArgs) => {
-            return new curried(...args,...newArgs)
-        }
-    }
-}
+ }
+ 
+ const curriedJoin = curry(join)
+ const _ = curry.placeholder
+ 
+ console.log(curriedJoin(1, 2, 3)) // '1_2_3'
+ 
+ console.log(curriedJoin(_, 2)(1, 3)) // '1_2_3'
+ 
+ console.log(curriedJoin(_, _, _)(1)(_, 3)(2)) // '1_2_3'
