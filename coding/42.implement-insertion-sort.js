3a67aaff44036e014bd7931e75e2b01dd69bcf6d
apoorvasa03 <apoorvasa03@gmail.com>
2022-02-15 09:17:33 -0800

    - sort

---
+++ coding/42.implement-insertion-sort.js
@@ -0,0 +1,18 @@
+
+
+/** find the smallest and swap the num at i to the smallest num index */
+function insertionSort(arr){
+    for(let i=1; i< arr.length; i++){
+        for(let j = i; j > 0; j--){
+            if(arr[j] < arr[j-1]){
+                [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+            }else{
+                break;
+            }
+        }
+    }
+
+    return arr
+}
+
+console.log(insertionSort([4,6,2,5,1,3]))
--- coding/44.implement-selection-sort.js
+++ coding/44.implement-selection-sort.js
@@ -1,5 +1,6 @@
+/** find the smallest num and move it to front of the array and 
+ * since the smallest is in the front of the array we will not consider it in next iteration */
 function selectionSort(arr){
-
     for(let i =0; i < arr.length; i++){
         let minIndex = i
         for(let j = i+1; j< arr.length; j++){
