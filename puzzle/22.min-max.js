9ed0c6c69ab7158db24fe27f659e991bf22429d7
apoorvasa03 <apoorvasa03@gmail.com>
2022-03-04 11:51:08 -0800

    - puzzle

---
+++ puzzle/22.min-max.js
@@ -0,0 +1,18 @@
+/** min/max converts values to number */
+
+console.log(Math.min())
+console.log(Math.max())
+console.log(Math.min(1))
+console.log(Math.max(1,2))
+console.log(Math.min([1,2,3]))
+console.log(Math.max([1],[2],[3]))
+
+
+/**
+ Infinity
+ -Infinity
+ 1
+ 2
+ NaN
+ 3
+ */
