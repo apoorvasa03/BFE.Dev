c8a05e220f420dee801435e977fbaca5aeaff215
apoorvasa03 <apoorvasa03@gmail.com>
2022-04-04 12:59:49 -0700

    - flat array and object

--- coding/3.implement-array.prototype.flat.js
+++ coding/3.implement-array.prototype.flat.js
@@ -11,10 +11,43 @@ function flat(arr, depth = 1){
     return result
 }
 
+/** -------flaten the object ------ */
+function flattenDictionary(dict){
+    let flatDictionary = {}
+    flattenDictionaryHelper('', dict, flatDictionary)
+    return flatDictionary
+  }
+  
+  function flattenDictionaryHelper(initailKey, dict, flatDictionary){
+    for(let key in dict){
+      let value = dict[key]
+      let updatedInitialKey = initailKey ? initailKey + (key ? ('.' + key) : '') : key
+  
+      if(typeof value != 'object' && typeof value != 'function'){
+        flatDictionary[updatedInitialKey] = value
+      }else{
+        flattenDictionaryHelper(updatedInitialKey, value, flatDictionary)
+      }
+    }
+  }
+  
+  let dict = {
+    "Key1" : "1",
+    "Key2" : {
+        "a" : "2",
+        "b" : "3",
+        "c" : {
+            "d" : "3",
+            "e" : {
+                "" : "1"
+            }
+        }
+    }
+  }
+
 const arr = [1, [2], [3, [4]]];
-
 console.log(flat(arr)) // [1, 2, 3, [4]]
-
 console.log(flat(arr, 1)) // [1, 2, 3, [4]]
-
 console.log(flat(arr, 2)) // [1, 2, 3, 4]
+  
+console.log(flattenDictionary(dict))
